952661f50a9bd4464b5f3e3d47c55da94163cca0
bossagyu <feel-the-wind.sakura@hotmail.co.jp>
2023-01-14 12:02:52 +0900

    add section24

---
+++ JavaScript/section24/Manipulating/app.js
@@ -0,0 +1,12 @@
+const allLinks = document.querySelectorAll('a');
+
+// for (let link of allLinks) {
+//     link.innerText = '私はリンクです！！！！';
+// }
+
+for (let link of allLinks) {
+    link.style.color = 'green';
+    link.style.textDecorationColor = 'magenta';
+    link.style.textDecorationStyle = 'wavy';
+}
+link.st
