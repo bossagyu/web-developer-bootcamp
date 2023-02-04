a2d402f0641d36771141c20f7296b5be0c702c5d
bossagyu <feel-the-wind.sakura@hotmail.co.jp>
2023-02-04 17:19:18 +0900

    expressのget, postのサンプルを追加

---
+++ NodeJs/section33/expressSample/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+// app.use((req, res)=> {
+//     console.log('リクエストを受付マシア');
+//     console.log(req)
+//     res.send('response')
+// })
+
+// expressのルーティングは上から見ていく
+
+app.get('/cats', (req, res) => {
+    res.send('にゃー');
+});
+
+app.get('/dogs', (req, res) => {
+    res.send('わんわん');
+})
+
+app.post('/dogs', (req, res) => {
+    res.send('わんわん');
+})
+
+app.get('/', (req, res) => {
+    res.send('home');
+})
+
+app.get('*', (req, res) => {
+    res.send('そんなパスはありません');
+})
+
+app.listen(3000, () => {
+    console.log('aaa');
+})
