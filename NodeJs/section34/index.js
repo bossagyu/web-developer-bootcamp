abd2525bdebd101327f3509373a255e0be4c7154
bossagyu <feel-the-wind.sakura@hotmail.co.jp>
2023-02-06 09:51:00 +0900

    ejsの中にfor文を書く方法を追加

--- NodeJs/section34/index.js
+++ NodeJs/section34/index.js
@@ -7,13 +7,22 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('home');
-})
+});
 
 app.get('/rand', (req, res) => {
     const num = Math.floor((Math.random() * 10) + 1);
     console.log(num);
     res.render('rand', {random: num});
-})
+});
+
+app.get('/cats', (req, res) => {
+    console.log('aaaaaaaaa')
+    const cats = [
+        'たま', 'トラ', '黒','w'
+    ];
+    res.render('cats', {cats});
+});
+
 
 app.listen(3000, () => {
     console.log('ポート3000で待受中');
