8b950879f87edcf2604a270ae8337c8951c80185
bossagyu <feel-the-wind.sakura@hotmail.co.jp>
2023-02-16 10:16:07 +0900

    postで受け取ったリクエストをパースする方法を追加

--- NodeJs/section35/Lesson1/index.js
+++ NodeJs/section35/Lesson1/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 app.get('/tacos', (req, res) => {
     res.send('GET /tacos response');
 });
 
 app.post('/tacos', (req, res) => {
-    res.send('POST /tacos response');
+    const { meat, qty } = req.body;
+    res.send(`${meat}を${qty}どうぞ`);
 });
 
 app.listen(3000, () => {
